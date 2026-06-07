984b1f664fdfd6dce6994df402f4415a552f5a14
Divyansh <divyanshchauhan349@gmail.com>
2026-06-07 10:23:32 +0530

    fix: add CORS origin for production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const expenseRoutes = require('./routes/expenses');
 const app = express();
 const PORT = 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://mini-expense-tracker-green.vercel.app'
+}));
 app.use(express.json());
 
 app.use('/api/expenses', expenseRoutes);
